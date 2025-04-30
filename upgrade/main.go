c04d480126c3190ad4ecc6634659e7d134f39a97
chandankumar4 <chandan.kr404@gmail.com>
2025-04-30 20:49:29 +0530

    Update crds file
    Signed-off-by: chandankumar4 <chandan.kr404@gmail.com>

--- upgrade/main.go
+++ upgrade/main.go
@@ -14,7 +14,9 @@ func main() {
 		log.Fatalln("Numaflow version is required as a command line argument")
 	}
 
-	fmt.Println("################### Updating latest data for RBAC ###################")
+	fmt.Println("################### Updating latest CRDs ###################")
+	internal.UpdateCRDFiles(numaflowVersion)
+	fmt.Println("\n################### Updating latest data for RBAC ###################")
 	internal.UpdateRBACFiles(numaflowVersion)
 	fmt.Println("\n################### Updating latest data for Service Account ###################")
 	internal.UpdateServiceAccount(numaflowVersion)
